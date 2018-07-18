ae3e719e5c8fa77950abf75cec9948c508cc6344
Gabriel César <gcal1@live.com>
2018-07-18 11:01:01 -0300

    add webview

--- first-project/main.js
+++ first-project/main.js
@@ -3,10 +3,12 @@ const { app, BrowserWindow } = require('electron');
 
 const appUrl = `file://${__dirname}/index.html`;
 
-let win;
+let win, child;
 
 const createElectronShell = () => {
 	win = new BrowserWindow({ width: 800, height: 600 });
+	child = new BrowserWindow({ width: 600, height: 400, parent: win, modal: true });
+
 	win.loadURL(appUrl);
 	win.on('closed', () => { win = null; });
 };
