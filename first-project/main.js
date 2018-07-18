d7e84c619222dd02a2910ef22d503bce7fad9025
Gabriel César <gcal1@live.com>
2018-07-18 10:16:14 -0300

    initial project

---
+++ first-project/main.js
@@ -0,0 +1,18 @@
+// initial project 
+const { app, BrowserWindow } = require('electron');
+
+const appUrl = `file://${__dirname}/index.html`;
+
+let win;
+
+const createElectronShell = () => {
+	win = new BrowserWindow({ width: 800, height: 600 });
+	win.loadURL(appUrl);
+	win.on('closed', () => { win = null; });
+};
+
+app.on('ready', createElectronShell);
+
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') { app.quit(); } })
+
+app.on('activate', () => { if (app == null) { createElectronShell() } })
